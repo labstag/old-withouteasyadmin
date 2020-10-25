4051d2aae2ae0dfce3e85f0fa43501219e99d7af
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2020-10-25 20:04:55 +0100

    :alembic: alembic (33): Ajout bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
-let Encore = require('@symfony/webpack-encore');
-let dotenv = require ("dotenv");
-let fs = require('fs');
+var Encore = require('@symfony/webpack-encore');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -10,11 +8,11 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 
 Encore
     // directory where compiled assets will be stored
-    .setOutputPath('apps/public/build/')
+    .setOutputPath('apps/public/assets/')
     // public path used by the web server to access the output path
-    .setPublicPath('/build')
+    .setPublicPath('/assets')
     // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
+    .setManifestKeyPrefix('assets/')
 
     /*
      * ENTRY CONFIG
@@ -23,9 +21,10 @@ Encore
      * (including one that's included on every page - e.g. "app")
      *
      * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.scss) if your JavaScript imports CSS.
+     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('app', ['./assets/app.js', './assets/app.scss'])
+    .addEntry('admin', ['./assets/admin.js', './assets/admin.scss'])
     //.addEntry('page1', './assets/page1.js')
     //.addEntry('page2', './assets/page2.js')
 
@@ -35,7 +34,10 @@ Encore
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
-
+    .configureUrlLoader({
+        fonts: { limit: 4096 },
+        images: { limit: 4096 }
+    })
     /*
      * FEATURE CONFIG
      *
@@ -63,10 +65,10 @@ Encore
 
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes(Encore.isProduction())
+    .enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    .autoProvidejQuery()
+    //.autoProvidejQuery()
 
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
