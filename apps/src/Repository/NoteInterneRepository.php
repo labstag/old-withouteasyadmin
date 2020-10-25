89a5b88557415df375db2f0f5481480db06c02e3
Le TOULLEC Martial <letoullec.martial@gmail.com>
2020-10-25 15:17:52 +0100

    :alembic: alembic (21): CRUD entity note interne (#25)
    * :alembic: alembic (21): CRUD entity note interne
    
    * Restyle :alembic: alembic (21): CRUD entity note interne (#26)
    
    * Restyled by php-cs-fixer
    
    * Restyled by whitespace
    
    * Update NoteInterneController.php
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    Co-authored-by: Le TOULLEC Martial <letoullec.martial@gmail.com>
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>

---
+++ apps/src/Repository/NoteInterneRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Labstag\Repository;
+
+use Labstag\Entity\NoteInterne;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method NoteInterne|null find($id, $lockMode = null, $lockVersion = null)
+ * @method NoteInterne|null findOneBy(array $criteria, array $orderBy = null)
+ * @method NoteInterne[]    findAll()
+ * @method NoteInterne[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class NoteInterneRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, NoteInterne::class);
+    }
+
+    // /**
+    //  * @return NoteInterne[] Returns an array of NoteInterne objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('n')
+            ->andWhere('n.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('n.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?NoteInterne
+    {
+        return $this->createQueryBuilder('n')
+            ->andWhere('n.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
