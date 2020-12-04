775df7b10e2c0ee4beaf7b881b616fb63daf6d8f
Restyled.io <commits@restyled.io>
2020-12-04 19:01:00 +0000

    Restyled by clang-format

--- assets/admin.js
+++ assets/admin.js
@@ -1,6 +1,5 @@
 import * as assets from './assets';
-function clickDeleteEntity(event)
-{
+function clickDeleteEntity(event) {
   event.preventDefault();
   let url = event.currentTarget.dataset.url;
   let token = event.currentTarget.dataset.token;
@@ -11,11 +10,10 @@ function clickDeleteEntity(event)
   btnConfirm.dataset.redirect = redirect;
 }
 
-function clickFormSave(event)
-{
+function clickFormSave(event) {
   event.preventDefault();
   const formId = event.currentTarget.dataset.form;
-  document.querySelector("form[name='"+formId+"']").submit();
+  document.querySelector("form[name='" + formId + "']").submit();
 }
 
 function clickbtnConfirmDelete(event) {
@@ -23,53 +21,39 @@ function clickbtnConfirmDelete(event) {
   const url = element.dataset.url;
   const token = element.dataset.token;
   const redirect = element.dataset.redirect;
-  const data = {
-    '_token': token
-  };
-  const searchParams = Object.keys(data).map((key) => {
-    return encodeURIComponent(key) + '=' + encodeURIComponent(data[key]);
-  }).join('&');
+  const data = {'_token' : token};
+  const searchParams = Object.keys(data)
+                           .map((key) => {
+                             return encodeURIComponent(key) + '=' +
+                                    encodeURIComponent(data[key]);
+                           })
+                           .join('&');
   let options = {
-    method: 'POST',
-    headers: {
-      "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
-    },
-    body: searchParams
+    method : 'POST',
+    headers :
+        {"Content-type" : "application/x-www-form-urlencoded; charset=UTF-8"},
+    body : searchParams
   };
-  fetch(
-    url,
-    options
-  ).then(
-    response => {
-      window.location.href = redirect;
-    }
-  ).catch(
-    err => {
-      console.log(err)
-    }
-  );
+  fetch(url, options)
+      .then(response => { window.location.href = redirect; })
+      .catch(err => {console.log(err)});
 }
 let saveForm = document.querySelectorAll('#SaveForm');
 if (saveForm.length) {
-  saveForm.forEach(element => {
-    element.addEventListener('click', clickFormSave);
-  })
+  saveForm.forEach(
+      element => { element.addEventListener('click', clickFormSave); })
 }
 let btnConfirmDeletes = document.querySelectorAll('.BtnConfirmDelete');
 if (btnConfirmDeletes.length) {
-  btnConfirmDeletes.forEach(element => {
-    element.addEventListener('click', clickbtnConfirmDelete);
-  });
+  btnConfirmDeletes.forEach(
+      element => { element.addEventListener('click', clickbtnConfirmDelete); });
 }
 let deleteForms = document.querySelectorAll('#DeleteForm');
 if (deleteForms.length) {
-  deleteForms.forEach(element => {
-    element.addEventListener('click', clickDeleteEntity);
-  });
+  deleteForms.forEach(
+      element => { element.addEventListener('click', clickDeleteEntity); });
 }
 
 window.operateEvents = {
-  'click .btnAdminTableDelete': function (event) {
-    clickDeleteEntity(event);
-  }
+  'click .btnAdminTableDelete' : function(event) { clickDeleteEntity(event); }
 }
--- assets/assets.js
+++ assets/assets.js
@@ -4,25 +4,23 @@ require('select2');
 import bsCustomFileInput from 'bs-custom-file-input';
 bsCustomFileInput.init();
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-function clickToggleFieldset(event)
-{
+function clickToggleFieldset(event) {
   event.preventDefault();
   let ihtmls = event.currentTarget.querySelectorAll('i');
   let className = 'd-none';
-  ihtmls.forEach(
-    ihtml => {
-      if (ihtml.classList.contains(className)) {
-        ihtml.classList.remove(className);
-      } else {
-        ihtml.classList.add(className);
-      }
+  ihtmls.forEach(ihtml => {
+    if (ihtml.classList.contains(className)) {
+      ihtml.classList.remove(className);
+    } else {
+      ihtml.classList.add(className);
     }
-  )
+  })
   let fieldset = event.currentTarget.closest('fieldset');
   let fieldrow = fieldset.querySelector('.FieldRow');
   let btnCollectionAdd = fieldset.querySelector('.BtnCollectionAdd');
   if (btnCollectionAdd != null) {
-    if (btnCollectionAdd.style.display == '' || btnCollectionAdd.style.display == 'block') {
+    if (btnCollectionAdd.style.display == '' ||
+        btnCollectionAdd.style.display == 'block') {
       btnCollectionAdd.style.display = 'none';
     } else {
       btnCollectionAdd.style.display = 'block';
@@ -34,22 +32,18 @@ function clickToggleFieldset(event)
     fieldrow.style.display = 'block';
   }
 }
-function clickEnableDeleteCollection(event)
-{
+function clickEnableDeleteCollection(event) {
   event.preventDefault();
   let CollectionRow = event.currentTarget.closest('.CollectionRow');
   CollectionRow.parentNode.removeChild(CollectionRow);
 }
 function EnableDeleteCollection() {
   let btnCollectionDeletes = document.querySelectorAll('.BtnCollectionDelete');
-  btnCollectionDeletes.forEach(
-    btnCollectionDelete => {
-      btnCollectionDelete.addEventListener('click', clickEnableDeleteCollection);
-    }
-  );
+  btnCollectionDeletes.forEach(btnCollectionDelete => {
+    btnCollectionDelete.addEventListener('click', clickEnableDeleteCollection);
+  });
 }
-function clickbtnCollectionAdd(event)
-{
+function clickbtnCollectionAdd(event) {
   event.preventDefault();
   let fieldset = event.currentTarget.closest('fieldset');
   let counter = fieldset.querySelectorAll('.CollectionRow').length;
@@ -61,35 +55,25 @@ function clickbtnCollectionAdd(event)
 let wysiwygs = document.querySelectorAll('.wysiwyg');
 if (wysiwygs.length) {
   wysiwygs.forEach(element => {
-    ClassicEditor.create(element).then( editor => {
-        window.editor = editor;
-    } ).catch( error => {
-        console.error( 'There was a problem initializing the editor.', error );
-    } );
+    ClassicEditor.create(element)
+        .then(editor => { window.editor = editor; })
+        .catch(error => {
+          console.error('There was a problem initializing the editor.', error);
+        });
   });
 }
 EnableDeleteCollection();
 let btnCollectionAdds = document.querySelectorAll('.BtnCollectionAdd');
-btnCollectionAdds.forEach(
-  btnCollectionAdd => {
-    btnCollectionAdd.addEventListener('click', clickbtnCollectionAdd);
-  }
-);
+btnCollectionAdds.forEach(btnCollectionAdd => {
+  btnCollectionAdd.addEventListener('click', clickbtnCollectionAdd);
+});
 let toggleFieldsets = document.querySelectorAll('.ToggleFieldset');
-toggleFieldsets.forEach(
-  toggleFieldset => {
-    toggleFieldset.addEventListener('click', clickToggleFieldset);
-  }
-);
+toggleFieldsets.forEach(toggleFieldset => {
+  toggleFieldset.addEventListener('click', clickToggleFieldset);
+});
 
 let selects = document.querySelectorAll('select');
-selects.forEach(
-  select => {
-    let id = select.getAttribute('id');
-    $("#" + id).select2(
-      {
-        theme: 'bootstrap4'
-      }
-    );
-  }
-)
+selects.forEach(select => {
+  let id = select.getAttribute('id');
+  $("#" + id).select2({theme : 'bootstrap4'});
+})
