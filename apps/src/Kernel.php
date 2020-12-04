cd6dc0cb053ca5efdeac1bfc0a69fce7aeced3dc
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2020-12-04 20:42:26 +0100

    :arrow_up: arrow_up: update recipes

--- apps/src/Kernel.php
+++ apps/src/Kernel.php
@@ -23,7 +23,7 @@ class Kernel extends BaseKernel
             $container->import('../config/services.yaml');
             $container->import('../config/{services}_'.$this->environment.'.yaml');
         } elseif (is_file($path = \dirname(__DIR__).'/config/services.php')) {
-            (include $path)($container->withPath($path), $this);
+            (require $path)($container->withPath($path), $this);
         }
     }
 
@@ -35,7 +35,7 @@ class Kernel extends BaseKernel
         if (is_file(\dirname(__DIR__).'/config/routes.yaml')) {
             $routes->import('../config/routes.yaml');
         } elseif (is_file($path = \dirname(__DIR__).'/config/routes.php')) {
-            (include $path)($routes->withPath($path), $this);
+            (require $path)($routes->withPath($path), $this);
         }
     }
 }
