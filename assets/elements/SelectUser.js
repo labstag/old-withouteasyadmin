2cf9334b1d2aad1a0c009f47d87bf44890686a9f
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2021-01-12 14:37:53 +0100

    :alembic: alembic: Add custom element select-user

---
+++ assets/elements/SelectUser.js
@@ -0,0 +1,9 @@
+require("select2");
+export class SelectUser extends HTMLSelectElement{
+  connectedCallback()
+  {
+    console.log('select user');
+    const id = this.getAttribute("id");
+    $("#" + id).select2({ theme: "bootstrap4" });
+  }
+}
