0e40eeb0315808c548057299f95611ece43ef290
Restyled.io <commits@restyled.io>
2021-01-24 21:33:01 +0000

    Restyled by clang-format

--- assets/elements/SelectRefUser.js
+++ assets/elements/SelectRefUser.js
@@ -1,26 +1,19 @@
 require("select2");
-export class SelectRefUser extends HTMLSelectElement
-{
-  connectedCallback()
-  {
+export class SelectRefUser extends HTMLSelectElement {
+  connectedCallback() {
     console.log('select user');
     const id = this.getAttribute("id");
     $("#" + id).select2({
-      theme: "bootstrap4",
-      ajax: {
-        url: this.dataset.url,
-        data: function (params)
-        {
-          let query = {
-            name: params.term
-          };
+      theme : "bootstrap4",
+      ajax : {
+        url : this.dataset.url,
+        data : function(params) {
+          let query = {name : params.term};
 
           return query;
         },
-        dataType: 'json'
+        dataType : 'json'
       }
     });
-
-
   }
 }
