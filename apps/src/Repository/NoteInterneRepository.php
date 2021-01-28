c4ecfb2df04d3e188b8fb46b95c9d92862614a98
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2021-01-28 16:10:23 +0100

    wip(crud): correction adresse user (#132)
    * wip(crud): correction adresse user
    
    * wip(crud): correction list
    
    * wip(crud): correction twig

--- apps/src/Repository/NoteInterneRepository.php
+++ apps/src/Repository/NoteInterneRepository.php
@@ -2,6 +2,7 @@
 
 namespace Labstag\Repository;
 
+use Doctrine\ORM\Query;
 use Doctrine\Persistence\ManagerRegistry;
 use Labstag\Entity\NoteInterne;
 use Labstag\Lib\ServiceEntityRepositoryLib;
@@ -11,5 +12,45 @@ class NoteInterneRepository extends ServiceEntityRepositoryLib
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, NoteInterne::class);
+    }
+
+    public function findAllForAdmin(): Query
+    {
+        $queryBuilder = $this->createQueryBuilder('a');
+        $query        = $queryBuilder->leftJoin(
+            'a.refuser',
+            'u'
+        );
+        $query->where(
+            'u.deletedAt=:userDeleteAt AND a.deletedAt=:adresseDeleteAt'
+        );
+        $query->setParameters(
+            [
+                'userDeleteAt'    => '',
+                'adresseDeleteAt' => '',
+            ]
+        );
+
+        return $query->getQuery();
+    }
+
+    public function findTrashForAdmin(): Query
+    {
+        $queryBuilder = $this->createQueryBuilder('a');
+        $query        = $queryBuilder->leftJoin(
+            'a.refuser',
+            'u'
+        );
+        $query->where(
+            'u.deletedAt!=:userDeleteAt OR a.deletedAt!=:adresseDeleteAt'
+        );
+        $query->setParameters(
+            [
+                'userDeleteAt'    => '',
+                'adresseDeleteAt' => '',
+            ]
+        );
+
+        return $query->getQuery();
     }
 }
