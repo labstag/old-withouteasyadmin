d4e62e58ec34ed51de3a7214a718e55031736548
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2021-02-10 16:14:38 +0100

    fix PHP

---
+++ apps/src/Controller/Api/GuardUserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Labstag\Controller\Api;
+
+use Labstag\Entity\User;
+use Labstag\Lib\ApiControllerLib;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/api/guard/user")
+ */
+class GuardUserController extends ApiControllerLib
+{
+}
