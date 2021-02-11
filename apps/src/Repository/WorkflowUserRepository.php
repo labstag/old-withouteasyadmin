5e519fc2957b42596411b596ac83503ee054c98c
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2021-02-11 15:11:11 +0100

    wip(guard): changement vue Route

--- apps/src/Repository/WorkflowUserRepository.php
+++ apps/src/Repository/WorkflowUserRepository.php
@@ -3,6 +3,7 @@
 namespace Labstag\Repository;
 
 use Doctrine\Persistence\ManagerRegistry;
+use Labstag\Entity\User;
 use Labstag\Entity\WorkflowUser;
 use Labstag\Lib\ServiceEntityRepositoryLib;
 
@@ -11,5 +12,22 @@ class WorkflowUserRepository extends ServiceEntityRepositoryLib
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, WorkflowUser::class);
+    }
+
+    public function findEnable(?User $user = null)
+    {
+        $queryBuilder = $this->createQueryBuilder('a');
+        $query        = $queryBuilder->where(
+            'a.state=:state'
+        );
+        $parameters   = ['state' => 1];
+        if (!is_null($user)) {
+            $query->andWhere('a.refuser=:refuser');
+            $parameters['refuser'] = $user;
+        }
+
+        $query->setParameters($parameters);
+
+        return $query->getQuery()->getResult();
     }
 }
