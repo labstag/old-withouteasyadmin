2577f4729d36e7aa8f5ea1367317f97561d68a29
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2021-03-15 13:59:07 +0100

    fix: fields

--- apps/src/Entity/User.php
+++ apps/src/Entity/User.php
@@ -270,7 +270,7 @@ class User implements UserInterface, Serializable
         // $this->plainPassword = null;
     }
 
-    public function getRefGroupe(): ?Groupe
+    public function getRefgroupe(): ?Groupe
     {
         return $this->refgroupe;
     }
