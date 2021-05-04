1eace752cb517c096cf54b276d0817cc471c7573
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2021-05-04 21:23:16 +0200

    feat(post): admin post

--- apps/migrations/2021/05/Version20210504190202.php
+++ apps/migrations/2021/05/Version20210504190202.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210326102638 extends AbstractMigration
+final class Version20210504190202 extends AbstractMigration
 {
     public function getDescription() : string
     {
