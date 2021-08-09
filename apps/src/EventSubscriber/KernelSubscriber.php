7c929d54af18939569064b7eacc28cb42afd6a66
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2021-08-09 23:27:54 +0200

    fix(pph): use linter

--- apps/src/EventSubscriber/KernelSubscriber.php
+++ apps/src/EventSubscriber/KernelSubscriber.php
@@ -7,21 +7,79 @@ use tidy;
 
 class KernelSubscriber implements EventSubscriberInterface
 {
+    public const TAGS = [
+        'workflow-action',
+        'link-show',
+        'link-guard',
+        'link-edit',
+        'link-delete',
+        'link-restore',
+        'link-destroy',
+        'link-trash',
+        'link-btnadmin',
+        'link-empty',
+        'link-btnadminempty',
+        'link-btnadminempties',
+        'link-btnadmindeleties',
+        'link-btnadminemptyall',
+        'link-btnadminrestore',
+        'link-btnadminrestories',
+        'link-btnadmindestroy',
+        'link-btnadmindelete',
+        'guard-route',
+        'guard-workflow',
+        'guard-setworkflow',
+        'guard-setroute',
+        'guard-allworkflow',
+        'guard-allroute',
+        'guard-changeworkflow',
+        'guard-changeroute',
+        'guard-refgrouproute',
+        'guard-refgroupworkflow',
+        'confirm-delete',
+        'confirm-deleteattachment',
+        'confirm-destroy',
+        'confirm-restore',
+        'confirm-restories',
+        'confirm-empty',
+        'confirm-emptyall',
+        'confirm-empties',
+        'confirm-deleties',
+        'confirm-workflow',
+        'attachment-img',
+        'attachment-delete',
+        'btn-addcollection',
+        'btn-delete',
+        'btn-togglefieldset',
+        'select-country',
+        'select-selector',
+        'select-refuser',
+        'input-phone',
+        'input-email',
+        'input-url',
+        'input-gps',
+        'input-codepostal',
+        'input-ville',
+        'table-datatable',
+        'select-all',
+        'select-element',
+    ];
+
     public function onKernelResponse($event)
     {
-        $request                   = $event->getRequest();
-        $response                  = $event->getResponse();
-        $content                   = $response->getContent();
-        $config = [
-            'indent'         => true,
-            'indent-spaces'  => 2,
-            'output-xhtml'   => true,
-            'drop-empty-elements' => false,
+        $request  = $event->getRequest();
+        $response = $event->getResponse();
+        $content  = $response->getContent();
+        $config   = [
+            'indent'                      => true,
+            'indent-spaces'               => 2,
+            'output-xhtml'                => true,
+            'drop-empty-elements'         => false,
             'drop-proprietary-attributes' => false,
-            'new-inline-tags' => 'workflow-action link-show link-guard link-edit link-delete link-restore link-destroy link-trash link-btnadmin link-empty link-btnadminempty link-btnadminempties link-btnadmindeleties link-btnadminemptyall link-btnadminrestore link-btnadminrestories link-btnadmindestroy link-btnadmindelete guard-route guard-workflow guard-setworkflow guard-setroute guard-allworkflow guard-allroute guard-changeworkflow guard-changeroute guard-refgrouproute guard-refgroupworkflow confirm-delete confirm-deleteattachment confirm-destroy confirm-restore confirm-restories confirm-empty confirm-emptyall confirm-empties confirm-deleties confirm-workflow attachment-img attachment-delete btn-addcollection btn-delete btn-togglefieldset select-country select-selector select-refuser input-phone input-email input-url input-gps input-codepostal input-ville table-datatable select-all select-element ',
-            'wrap'           => 200,
+            'new-inline-tags'             => implode(' ', self::TAGS),
+            'wrap'                        => 200,
         ];
-        $tidy = new tidy();
+        $tidy     = new tidy();
         $tidy->parseString($content, $config, 'utf8');
         $tidy->cleanRepair();
         $response->setContent($tidy);
@@ -30,8 +88,6 @@ class KernelSubscriber implements EventSubscriberInterface
 
     public static function getSubscribedEvents()
     {
-        return [
-            'kernel.response' => 'onKernelResponse',
-        ];
+        return ['kernel.response' => 'onKernelResponse'];
     }
 }
