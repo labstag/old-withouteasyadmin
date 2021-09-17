7c1b6998067fb588776c4e786fb3667acbb6627c
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2021-09-17 14:04:17 +0200

    fix(category): reuse old templates PHP

--- apps/src/Repository/CategoryRepository.php
+++ apps/src/Repository/CategoryRepository.php
@@ -16,4 +16,38 @@ class CategoryRepository extends ServiceEntityRepositoryLib
     {
         parent::__construct($registry, Category::class);
     }
+
+    public function findAllParentForAdmin()
+    {
+        $methods = get_class_methods($this);
+        $name    = '';
+
+        if (in_array('getClassMetadata', $methods)) {
+            $name = $this->getClassMetadata()->getName();
+        }
+
+        $dql           = 'SELECT a FROM '.$name.' a WHERE a.parent IS NULL';
+        $entityManager = $this->getEntityManager();
+
+        return $entityManager->createQuery($dql);
+    }
+
+    public function findTrashParentForAdmin(): array
+    {
+        $methods = get_class_methods($this);
+        $name    = '';
+
+        if (in_array('getClassMetadata', $methods)) {
+            $name = $this->getClassMetadata()->getName();
+        }
+
+        $entityManager = $this->getEntityManager();
+        $dql           = $entityManager->createQueryBuilder();
+        $dql->select('a');
+        $dql->from($name, 'a');
+        $dql->where('a.deletedAt IS NOT NULL');
+        $dql->andwhere('a.parent IS NULL');
+
+        return $dql->getQuery()->getResult();
+    }
 }
