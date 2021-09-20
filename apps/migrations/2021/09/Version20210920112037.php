5780efaa3a709ec2c836480f64ffb58d7d25e63b
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2021-09-20 13:44:24 +0200

    feat(login): remember_me

--- apps/migrations/2021/09/Version20210920112037.php
+++ apps/migrations/2021/09/Version20210920112037.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210919224203 extends AbstractMigration
+final class Version20210920112037 extends AbstractMigration
 {
     public function getDescription(): string
     {
