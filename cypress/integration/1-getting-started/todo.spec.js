bf6b35c49cc06b687a8f92425fe2a867e5eb32c5
Restyled.io <commits@restyled.io>
2021-09-21 21:14:41 +0000

    Restyled by clang-format

--- cypress/integration/1-getting-started/todo.spec.js
+++ cypress/integration/1-getting-started/todo.spec.js
@@ -26,11 +26,11 @@ describe('example to-do app', () => {
     // which are the two default items.
     cy.get('.todo-list li').should('have.length', 2)
 
-    // We can go even further and check that the default todos each contain
-    // the correct text. We use the `first` and `last` functions
-    // to get just the first and last matched elements individually,
-    // and then perform an assertion with `should`.
-    cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
+  // We can go even further and check that the default todos each contain
+  // the correct text. We use the `first` and `last` functions
+  // to get just the first and last matched elements individually,
+  // and then perform an assertion with `should`.
+  cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
     cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
   })
 
@@ -38,19 +38,20 @@ describe('example to-do app', () => {
     // We'll store our item text in a variable so we can reuse it
     const newItem = 'Feed the cat'
 
-    // Let's get the input element and use the `type` command to
-    // input our new list item. After typing the content of our item,
-    // we need to type the enter key as well in order to submit the input.
-    // This input has a data-test attribute so we'll use that to select the
-    // element in accordance with best practices:
-    // https://on.cypress.io/selecting-elements
-    cy.get('[data-test=new-todo]').type(`${newItem}{enter}`)
+  // Let's get the input element and use the `type` command to
+  // input our new list item. After typing the content of our item,
+  // we need to type the enter key as well in order to submit the input.
+  // This input has a data-test attribute so we'll use that to select the
+  // element in accordance with best practices:
+  // https://on.cypress.io/selecting-elements
+  cy.get('[data-test=new-todo]').type(`${newItem}{enter}`)
 
-    // Now that we've typed our new item, let's check that it actually was added to the list.
-    // Since it's the newest item, it should exist as the last element in the list.
-    // In addition, with the two default items, we should have a total of 3 elements in the list.
-    // Since assertions yield the element that was asserted on,
-    // we can chain both of these assertions together into a single statement.
+  // Now that we've typed our new item, let's check that it actually was added
+  // to the list. Since it's the newest item, it should exist as the last
+  // element in the list. In addition, with the two default items, we should
+  // have a total of 3 elements in the list. Since assertions yield the element
+  // that was asserted on, we can chain both of these assertions together into a
+  // single statement.
     cy.get('.todo-list li')
       .should('have.length', 3)
       .last()
@@ -69,11 +70,12 @@ describe('example to-do app', () => {
       .find('input[type=checkbox]')
       .check()
 
-    // Now that we've checked the button, we can go ahead and make sure
-    // that the list element is now marked as completed.
-    // Again we'll use `contains` to find the <label> element and then use the `parents` command
-    // to traverse multiple levels up the dom until we find the corresponding <li> element.
-    // Once we get that element, we can assert that it has the completed class.
+  // Now that we've checked the button, we can go ahead and make sure
+  // that the list element is now marked as completed.
+  // Again we'll use `contains` to find the <label> element and then use the
+  // `parents` command to traverse multiple levels up the dom until we find the
+  // corresponding <li> element. Once we get that element, we can assert that it
+  // has the completed class.
     cy.contains('Pay electric bill')
       .parents('li')
       .should('have.class', 'completed')
@@ -96,15 +98,15 @@ describe('example to-do app', () => {
       // display only incomplete items
       cy.contains('Active').click()
 
-      // After filtering, we can assert that there is only the one
-      // incomplete item in the list.
-      cy.get('.todo-list li')
+    // After filtering, we can assert that there is only the one
+    // incomplete item in the list.
+    cy.get('.todo-list li')
         .should('have.length', 1)
         .first()
         .should('have.text', 'Walk the dog')
 
-      // For good measure, let's also assert that the task we checked off
-      // does not exist on the page.
+    // For good measure, let's also assert that the task we checked off
+    // does not exist on the page.
       cy.contains('Pay electric bill').should('not.exist')
     })
 
@@ -113,7 +115,7 @@ describe('example to-do app', () => {
       // that only completed tasks are shown
       cy.contains('Completed').click()
 
-      cy.get('.todo-list li')
+    cy.get('.todo-list li')
         .should('have.length', 1)
         .first()
         .should('have.text', 'Pay electric bill')
@@ -130,13 +132,13 @@ describe('example to-do app', () => {
       // Second, it selects the button so we can click it.
       cy.contains('Clear completed').click()
 
-      // Then we can make sure that there is only one element
-      // in the list and our element does not exist
-      cy.get('.todo-list li')
+    // Then we can make sure that there is only one element
+    // in the list and our element does not exist
+    cy.get('.todo-list li')
         .should('have.length', 1)
         .should('not.have.text', 'Pay electric bill')
 
-      // Finally, make sure that the clear button no longer exists.
+    // Finally, make sure that the clear button no longer exists.
       cy.contains('Clear completed').should('not.exist')
     })
   })
