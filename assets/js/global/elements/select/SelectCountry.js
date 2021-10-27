fa633ba2e5f8e23c728742bb44d45d525627bdb8
Restyled.io <commits@restyled.io>
2021-10-27 12:25:25 +0000

    Restyled by prettier

--- assets/js/global/elements/select/SelectCountry.js
+++ assets/js/global/elements/select/SelectCountry.js
@@ -1,23 +1,31 @@
-import TomSelect from 'tom-select'
+import TomSelect from "tom-select";
 export class SelectCountry extends HTMLSelectElement {
   connectedCallback() {
-    const idElement = this.getAttribute('id')
-    if (this.classList.contains('tomselected') === false) {
+    const idElement = this.getAttribute("id");
+    if (this.classList.contains("tomselected") === false) {
       this.select = new TomSelect(`#${idElement}`, {
-        sortField : {field : 'text', direction : 'asc'},
-        render : {
-          option : function(data, escape) {
-            return '<div><span class="flag-icon flag-icon-' +
-                   data.value.toLowerCase() + '"></span>' + escape(data.text) +
-                   '</div>'
+        sortField: { field: "text", direction: "asc" },
+        render: {
+          option: function (data, escape) {
+            return (
+              '<div><span class="flag-icon flag-icon-' +
+              data.value.toLowerCase() +
+              '"></span>' +
+              escape(data.text) +
+              "</div>"
+            );
           },
-          item : function(data, escape) {
-            return '<div><span class="flag-icon flag-icon-' +
-                   data.value.toLowerCase() + '"></span>' + escape(data.text) +
-                   '</div>'
-          }
-        }
-      })
+          item: function (data, escape) {
+            return (
+              '<div><span class="flag-icon flag-icon-' +
+              data.value.toLowerCase() +
+              '"></span>' +
+              escape(data.text) +
+              "</div>"
+            );
+          },
+        },
+      });
     }
   }
 }
