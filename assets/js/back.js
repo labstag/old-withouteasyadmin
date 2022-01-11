b36a03a4acb817d1204bd5cea51e227cf0367f5f
Restyled.io <commits@restyled.io>
2022-01-11 09:28:07 +0000

    Restyled by prettier

--- assets/js/back.js
+++ assets/js/back.js
@@ -1,25 +1,25 @@
-import './global'
-import './back/elements/index'
+import "./global";
+import "./back/elements/index";
 function clickFormSave(event) {
-  event.preventDefault()
-  const formId = event.currentTarget.getAttribute('form')
-  const formElement = document.querySelector("form[name='" + formId + "']")
+  event.preventDefault();
+  const formId = event.currentTarget.getAttribute("form");
+  const formElement = document.querySelector("form[name='" + formId + "']");
   if (formElement !== null) {
-    formElement.submit()
+    formElement.submit();
   }
 }
 
 function clickReset(event) {
-  event.preventDefault()
-  const element = event.currentTarget
-  window.location.href = element.closest('form').getAttribute('action')
+  event.preventDefault();
+  const element = event.currentTarget;
+  window.location.href = element.closest("form").getAttribute("action");
 }
 
-const saveForm = document.querySelector('#SaveForm')
+const saveForm = document.querySelector("#SaveForm");
 if (saveForm !== null) {
-  saveForm.addEventListener('click', clickFormSave)
+  saveForm.addEventListener("click", clickFormSave);
 }
-const resetForm = document.querySelector('#reset')
+const resetForm = document.querySelector("#reset");
 if (resetForm !== null) {
-  resetForm.addEventListener('click', clickReset)
+  resetForm.addEventListener("click", clickReset);
 }
