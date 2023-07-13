927bcb3a13cf7b11065f764155a050c4a6661094
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2023-07-13 16:00:55 +0200

    wip(storybook): with twig

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,7 @@
+const path = require('path');
+const srcDir = '../assets';
+const tplDir = '../apps/templates';
+
 /** @type { import('@storybook/web-components-webpack5').StorybookConfig } */
 const config = {
   stories: [
@@ -15,5 +19,43 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+  staticDirs: [
+    {
+      from: `${srcDir}/images`,
+      to: '/assets/images'
+    }
+  ],
+  async webpackFinal(config) {
+    config.module.rules = [
+      ...config.module.rules,
+      {
+        test: /\.twig$/,
+        use:  {
+          loader: 'twigjs-loader',
+          options: {
+            renderTemplate(twigData, dependencies, isHot) {
+              return `
+                ${dependencies}
+                const Twig = require('twig');
+                Twig.extendFilter('t', (value) => {
+                  return value;
+                });
+                const twig = Twig.twig;
+                const tpl = twig(${JSON.stringify(twigData)});
+                module.exports = function(context) { return tpl.render(context); };
+              `;
+            },
+          },
+        },
+      }
+    ];
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@fonts': path.resolve(__dirname, `${srcDir}/fonts`),
+      '@images': path.resolve(__dirname, `${srcDir}/images`),
+      '@theme': path.resolve(__dirname, tplDir),
+    };
+    return config;
+  }
 };
 export default config;
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,20 @@
 /** @type { import('@storybook/web-components').Preview } */
 const preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: {
+      argTypesRegex: "^on[A-Z].*"
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
+      },
+    },
+    options: {
+      storySort: {
+        method: 'alphabetical',
+        order: [ 'Introduction', 'Atoms', 'Molecules', 'Organisms', 'Templates', 'Pages' ],
+        locales: '',
       },
     },
   },
